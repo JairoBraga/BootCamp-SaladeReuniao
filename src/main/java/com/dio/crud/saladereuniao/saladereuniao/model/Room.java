a0f9d9ca9812202df5cacc460026ad77d8fc838a
JairoBraga <neto.braga08@gmail.com>
2020-11-20 23:58:38 -0300

    Exceptions

--- src/main/java/com/dio/crud/saladereuniao/saladereuniao/model/Room.java
+++ src/main/java/com/dio/crud/saladereuniao/saladereuniao/model/Room.java
@@ -1,4 +1,71 @@
 package com.dio.crud.saladereuniao.saladereuniao.model;
 
+import javax.persistence.*;
+
+@Table(name="meetingRoom")
+@Entity
 public class Room {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String name;
+    private String date;
+    private String startHour;
+    private String endHour;
+
+    public Room(long id, String name, String date, String startHour, String endHour) {
+        this.id = id;
+        this.name = name;
+        this.date = date;
+        this.startHour = startHour;
+        this.endHour = endHour;
+    }
+    public Room() {}
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    @Column(name="date",nullable = false)
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    @Column(name="startHour",nullable = false)
+    public String getStartHour() {
+        return startHour;
+    }
+
+    public void setStartHour(String startHour) {
+        this.startHour = startHour;
+    }
+
+    @Column(name="endHour",nullable = false)
+    public String getEndHour() {
+        return endHour;
+    }
+
+    public void setEndHour(String endHour) {
+        this.endHour = endHour;
+    }
+
+    @Override
+    public String toString() {
+        return "Room{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", date='" + date + '\'' +
+                ", startHour='" + startHour + '\'' +
+                ", endHour='" + endHour + '\'' +
+                '}';
+    }
 }
